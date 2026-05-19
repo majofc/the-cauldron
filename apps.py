ad617f7138ddcef54faa36439a9b6f091027854b
Majo <majofc1@gmail.com>
2026-05-18 21:41:34 -0600

    chore: initial Django app scaffold for the_cauldron

---
+++ apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class TheCauldronConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "the_cauldron"
+    verbose_name = "The Cauldron"
